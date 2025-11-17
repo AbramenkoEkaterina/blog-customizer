903db242639dd177d38ffd6839cfd4ec1746d57b
Abramenko Ekaterina <katiatgn@yandex.ru>
2025-11-17 18:10:04 +0300

    1

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,10 @@ import { Article } from './components/article/Article';
 //боковая панель с настройками
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
 //формальное описание структуры и обьект с начальными значениями
-import { defaultArticleState, ArticleStateType } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -16,54 +19,54 @@ const root = createRoot(domNode);
 
 export const App = () => {
 	//текущее состояние страницы, не начальное
-  const [articleState, setArticleState] = useState<ArticleStateType>(defaultArticleState);
-  //открытие/закрытие панели по умолчанию закрыта
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+	//открытие/закрытие панели по умолчанию закрыта
+	const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  //переключение сайдбара
-  const toggleSidebar = () => setSidebarOpen(prev => !prev);
-  const closeSidebar = () => setSidebarOpen(false);
+	//переключение сайдбара
+	const toggleSidebar = () => setSidebarOpen((prev) => !prev);
+	const closeSidebar = () => setSidebarOpen(false);
 
-  //применение изменений когда пользователь нажимает применить
-  const handleApply = (newState: ArticleStateType) => {
-    setArticleState(newState);//обновила состояние статьи
-    closeSidebar();//за это надо спросить нужент этот пункт или нет      ??????????
-  };
+	//применение изменений когда пользователь нажимает применить
+	const handleApply = (newState: ArticleStateType) => {
+		setArticleState(newState); //обновила состояние статьи
+		closeSidebar(); //за это надо спросить нужент этот пункт или нет      ??????????
+	};
 
-  //сбросить все настройки
-  const handleReset = (resetState: ArticleStateType) => {
-    setArticleState(resetState);
-  };
+	//сбросить все настройки
+	const handleReset = (resetState: ArticleStateType) => {
+		setArticleState(resetState);
+	};
 
-  return (
-    <main
-      className={clsx(styles.main)}
-      style={
-        {
-          '--font-family': articleState.fontFamilyOption.value,
-          '--font-size': articleState.fontSizeOption.value,
-          '--font-color': articleState.fontColor.value,
-          '--container-width': articleState.contentWidth.value,
-          '--bg-color': articleState.backgroundColor.value,
-        } as CSSProperties
-      }>
-      
-      <ArticleParamsForm
-        isOpen={sidebarOpen} //открыт/закрыт
-        initialState={articleState} //при открытие текущее состояние
-        onApply={handleApply}//те настроки которые выбраны до кнопки применить
-        onReset={handleReset}
-        onToggle={toggleSidebar}
-        onClose={closeSidebar}
-      />
-      
-      <Article />
-    </main>
-  );
+	return (
+		<main
+			className={clsx(styles.main)}
+			style={
+				{
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm
+				isOpen={sidebarOpen} //открыт/закрыт
+				initialState={articleState} //при открытие текущее состояние
+				onApply={handleApply} //те настроки которые выбраны до кнопки применить
+				onReset={handleReset}
+				onToggle={toggleSidebar}
+				onClose={closeSidebar}
+			/>
+
+			<Article />
+		</main>
+	);
 };
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
